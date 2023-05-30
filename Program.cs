3be956e5c86888ffd14edbe3ca39b10cfdac5e7f
felix adnel <flixadnel@gmail.com>
2023-05-29 22:20:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "Chachy";
+int age = 22;
+char middleInitial = 'a';
+bool isOver18 = true;
+double currentTemp = 60;
+decimal currentPrice = 77.07m;
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"middleinitial: {middleInitial}");
+Console.WriteLine($"isOver18: {isOver18}");
+Console.WriteLine($"currentTemp: {currentTemp}");
+Console.WriteLine($"currentPrice: {currentPrice}");
